a1519275befc5dd5b6c72e4ec8b703c92af552d7
Hasibull <hasibulislam4500@gmail.com>
2021-07-05 21:45:08 +0600

    solve some easy problems

---
+++ uri oj/uri_2615.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    cout<<"Uberlândia"<<endl;
+    cout<<"Canoas"<<endl;
+    cout<<"Ponta Grossa"<<endl;
+    cout<<"Várzea Grande"<<endl;
+    cout<<"Salvador"<<endl;
+
+    return 0;
+}
