f740da39e07ec52f47cd0ae4a4a06f3561492866
Hasibull <hasibulislam4500@gmail.com>
2022-01-07 20:58:02 +0600

    calculating the number of 5 and its powers

---
+++ CSES/1618.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n;
+
+    cin>>n;
+
+    int ans=0;
+
+    for(int i=5; i<=n; i*=5){
+        ans+=n/i;
+    }
+
+    cout<<ans<<endl;
+
+    return 0;
+}
