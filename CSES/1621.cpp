c61fccbc4be5874eec70d2318432ddcbecb0cee8
Hasibull <hasibulislam4500@gmail.com>
2022-01-09 23:10:59 +0600

    Easy one!

---
+++ CSES/1621.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n;
+
+    cin>>n;
+
+    set<int>st;
+
+    for(int i=0; i<n; i++){
+        int val;
+
+        cin>>val;
+
+        st.insert(val);
+    }
+
+    cout<<st.size()<<endl;
+
+    return 0;
+
+}
