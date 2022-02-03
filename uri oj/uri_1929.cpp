d3cf33add87f1562db71f94c4ae09bf1563ace97
Hasibull <hasibulislam4500@gmail.com>
2022-02-03 23:35:20 +0600

    Triangle related problem

---
+++ uri oj/uri_1929.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int a,b,c,d;
+
+    cin>>a>>b>>c>>d;
+
+    if(((a+b)>c && (a+c)>b && (b+c)>a) || ((a+b)>d && (a+d)>b && (b+d)>a) || ((a+c)>d && (a+d)>c && (c+d)>a) || ((b+d)>c && (b+c)>d && (c+d)>b)){
+        cout<<"S"<<endl;
+    }
+    else{
+        cout<<"N"<<endl;
+    }
+}
