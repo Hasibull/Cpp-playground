0b15165829127b68501128bc486c7045b0da0a85
Hasibull <hasibulislam4500@gmail.com>
2022-02-06 23:38:23 +0600

    Can't solved!

---
+++ uri oj/uri_1837.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int a,b;
+
+    cin>>a>>b;
+
+    if(a>0 && b>0){
+        cout<<(a/b)<<" "<<(a-((a/b)*b))<<endl;
+    }
+    else if(a>0 && b<0){
+        cout<<(a/b)<<" "<<(a-((a/b)*b))<<endl;
+    }
+    else if(a<0 && b>0){
+        if(a%b==0){
+            cout<<(a/b)<<" "<<"0"<<endl;
+        }
+        else{
+            cout<<(a/b)-1<<" "<<a+((1-(a/b))*b)<<endl;
+        }
+    }
+    else if(a<0 && b<0){
+        if(a%b==0){
+            cout<<(a/b)<<" "<<"0"<<endl;
+        }
+        else{
+            cout<<(a/b)+1<<" "<<a-((1+(a/b))*b)<<endl;
+        }
+    }
+
+    return 0;
+}
