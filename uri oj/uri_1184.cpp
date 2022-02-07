f7a169ca27015b7e49963ccb4df40446dcbb7627
Hasibull <hasibulislam4500@gmail.com>
2022-02-07 23:48:37 +0600

    Solved!

---
+++ uri oj/uri_1184.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    char ch;
+
+    cin>>ch;
+    cin.ignore();
+
+    double arr[13][13];
+
+    for(int i=0; i<12; i++){
+        for(int j=0; j<12; j++){
+            cin>>arr[i][j];
+        }
+    }
+
+    double sum=0.0;
+
+    for(int i=0; i<12; i++){
+        for(int j=0; j<i; j++){
+            sum+=arr[i][j];
+        }
+    }
+
+    if(ch=='S'){
+        cout<<fixed<<setprecision(1)<<sum<<endl;
+    }
+    else{
+        cout<<fixed<<setprecision(1)<<sum/66.0<<endl;
+    }
+
+    return 0;
+}
